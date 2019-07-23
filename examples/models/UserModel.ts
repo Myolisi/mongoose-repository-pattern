98127223f4fb73368e261add6709a58da996c5f7
myolisi <myolisi@ngubentombi.co.za>
2019-07-23 19:10:00 +0200

    Added some comments.

--- examples/models/UserModel.ts
+++ examples/models/UserModel.ts
@@ -3,8 +3,10 @@ import { CRUD } from '../../src/mongoose-crud';
 //if you dont want to create a typegoose schema use null
 export default class UserModel extends CRUD<null> {
   constructor() {
+    //here you pass the database and the collection
     super('mongo-crud', 'testingdb');
   }
+  //write your query
   getStuff() {
     return this.find().exec();
   }
